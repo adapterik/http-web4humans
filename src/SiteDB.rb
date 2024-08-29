7f2bea9614104f35b4024a0be3c518a4125bc05b
Erik Pearson <erik@adaptations.com>
2024-08-29 23:47:06 +0000

    add abstract to view

--- src/SiteDB.rb
+++ src/SiteDB.rb
@@ -202,6 +202,7 @@ class SiteDB
     # TODO: apply rules to fields
     
     title = changes['title']
+    abstract = changes['abstract']
     content = changes['content']
     content_type = changes['content_type']
 
@@ -209,10 +210,10 @@ class SiteDB
       
     query = '
       update content
-      set title = ?, content = ?, last_updated = ?, content_type = ?
+      set title = ?, content = ?, abstract = ?, last_updated = ?, content_type = ?
       where content.id = ?
     '
-    @db.execute query, [title, content, last_updated, content_type, content_id]
+    @db.execute query, [title, content, abstract, last_updated, content_type, content_id]
   end
 
   def add_content(content_item)
